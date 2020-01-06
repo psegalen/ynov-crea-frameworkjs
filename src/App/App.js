b751869115f291a7dbaf5d3f7bffac526807c87f
Pierre Segalen <pierre.segalen@gmail.com>
2020-01-06 17:46:51 +0100

    Added search feature

--- src/App/App.js
+++ src/App/App.js
@@ -19,15 +19,33 @@ class App extends React.Component {
     if (path.length > 0 && !isNaN(path)) {
       idArtist = path;
     }
+    this.loadArtist(idArtist);
+  }
+
+  loadArtist(idArtist) {
+    if (document.location.pathname.substr(1) !== idArtist) {
+      document.location.pathname = "/" + idArtist;
+    }
     axios
       .get("https://api.deezer.com/artist/" + idArtist)
       .then(response => this.setState({ data: response.data }));
   }
 
+  search(text) {
+    axios.get("https://api.deezer.com/search?q=" + text).then(response => {
+      var results = response.data.data;
+      if (results.length === 0) {
+        alert("No result for search!");
+      } else {
+        this.loadArtist(results[0].artist.id);
+      }
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <Header data={this.state.data} />
+        <Header data={this.state.data} onSearch={this.search.bind(this)} />
         <List idArtist={this.state.data ? this.state.data.id : null} />
         <Footer data={this.state.data} />
       </div>
--- src/App/Header/Header.js
+++ src/App/Header/Header.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 
 function Header(props) {
+  var [text, setText] = useState("");
   return (
     <div className="Header">
       <img
@@ -12,6 +13,15 @@ function Header(props) {
       <span className="Header-title">
         {props.data ? props.data.name : ""} discography
       </span>
+      <div>
+        Search{" "}
+        <input
+          type="text"
+          value={text}
+          onChange={evt => setText(evt.target.value)}
+        />{" "}
+        <button onClick={() => props.onSearch(text)}>Go!</button>
+      </div>
     </div>
   );
 }
