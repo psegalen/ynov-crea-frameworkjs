3723087144a99f0a7f88addb0ff9e2a25c4e1e4d
Pierre Segalen <pierre.segalen@gmail.com>
2020-01-06 11:59:16 +0100

    Turned Footer in a stateful component

--- src/App/Footer/Footer.js
+++ src/App/Footer/Footer.js
@@ -1,17 +1,19 @@
 import React from "react";
 import "./Footer.css";
 
-function Footer() {
-  return (
-    <div className="Footer">
-      <img
-        src="assets/deezer_logo.jpg"
-        className="Footer-Deezer-logo"
-        alt="Deezer logo"
-      />
-      <span className="Footer-title">Deezer info :</span>
-    </div>
-  );
+class Footer extends React.Component {
+  render() {
+    return (
+      <div className="Footer">
+        <img
+          src="assets/deezer_logo.jpg"
+          className="Footer-Deezer-logo"
+          alt="Deezer logo"
+        />
+        <span className="Footer-title">Deezer info :</span>
+      </div>
+    );
+  }
 }
 
 export default Footer;
